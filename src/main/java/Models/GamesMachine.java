bf077fea84c00ab3ce4b752749e0715b6d12ce61
seanm <20101936@mail.wit.ie>
2023-11-14 15:07:47 +0000

    Started Project

---
+++ src/main/java/Models/GamesMachine.java
@@ -0,0 +1,18 @@
+package Models;
+
+public class GamesMachine {
+
+    private String machineName = "";
+
+    private String manufacturer = "";
+
+    private String type = "";
+
+    private String media = "";
+
+    private int yearOfLaunch = 1900;
+
+    private double price = 00.00;
+
+    private String url = "";
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.retrovideogamesinformationsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.retrovideogamesinformationsystem to javafx.fxml;
+    exports com.example.retrovideogamesinformationsystem;
+}
