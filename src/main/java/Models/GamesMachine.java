585cbe3f4e57e86c94413b594dfbc10a31f87b54
seanm <20101936@mail.wit.ie>
2023-11-14 15:09:31 +0000

    Started Project

--- src/main/java/Models/GamesMachine.java
+++ src/main/java/Models/GamesMachine.java
@@ -15,4 +15,87 @@ public class GamesMachine {
     private double price = 00.00;
 
     private String url = "";
+
+    public GamesMachine(String machineName, String manufacturer, String type, String media, int yearOfLaunch, double price, String url) {
+        this.machineName = machineName;
+        this.manufacturer = manufacturer;
+        this.type = type;
+        this.media = media;
+        this.yearOfLaunch = yearOfLaunch;
+        this.price = price;
+        this.url = url;
+    }
+
+    public String getMachineName() {
+        return machineName;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getMedia() {
+        return media;
+    }
+
+    public int getYearOfLaunch() {
+        return yearOfLaunch;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setMachineName(String machineName) {
+        this.machineName = machineName;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setMedia(String media) {
+        this.media = media;
+    }
+
+    public void setYearOfLaunch(int yearOfLaunch) {
+        this.yearOfLaunch = yearOfLaunch;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+
+    public String toString() {
+        return "GamesMachine{" +
+                "machineName='" + machineName + '\'' +
+                ", manufacturer='" + manufacturer + '\'' +
+                ", type='" + type + '\'' +
+                ", media='" + media + '\'' +
+                ", yearOfLaunch=" + yearOfLaunch +
+                ", price=" + price +
+                ", url='" + url + '\'' +
+                '}';
+    }
 }
+
+
+
