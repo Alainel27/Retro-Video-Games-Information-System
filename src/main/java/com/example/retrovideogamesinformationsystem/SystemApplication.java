712fe9d290e624674db56df94b9fa21c094d3e49
Alainel27 <20102420@mail.wit.ie>
2023-11-16 11:44:30 +0000

    test1

--- src/main/java/com/example/retrovideogamesinformationsystem/SystemApplication.java
+++ src/main/java/com/example/retrovideogamesinformationsystem/SystemApplication.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SystemApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(SystemApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -21,3 +21,5 @@ public class HelloApplication extends Application {
         launch();
     }
 }
+
+//test
