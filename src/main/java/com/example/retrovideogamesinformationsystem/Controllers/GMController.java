f42ecc1179581846a6193528817dda895aafb6b8
Alan Z <20102420@wit.ie>
2023-11-18 18:45:13 +0000

    added GM FXML

--- src/main/java/com/example/retrovideogamesinformationsystem/Controllers/GMController.java
+++ src/main/java/com/example/retrovideogamesinformationsystem/Controllers/GMController.java
@@ -1,4 +1,13 @@
 package com.example.retrovideogamesinformationsystem.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class GMController {
+
+    @FXML
+    private Label test;
+
+
+
 }
