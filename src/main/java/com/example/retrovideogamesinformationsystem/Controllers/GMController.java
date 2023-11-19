f32efbf339a2a99e78479b2a69e32f71952939ec
seanm <20101936@mail.wit.ie>
2023-11-19 16:32:25 +0000

    Updated Controllers

--- src/main/java/com/example/retrovideogamesinformationsystem/Controllers/GMController.java
+++ src/main/java/com/example/retrovideogamesinformationsystem/Controllers/GMController.java
@@ -1,13 +1,53 @@
 package com.example.retrovideogamesinformationsystem.Controllers;
 
+import com.example.retrovideogamesinformationsystem.Models.GamesMachine;
+import com.example.retrovideogamesinformationsystem.Models.myLinkedList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 
 public class GMController {
 
+    private myLinkedList<GamesMachine> allGamesMachines = new myLinkedList<>();
+
+    public TextField addMachineName, addManufacturer, addType, addMedia, addYearOfRelease, addPrice, addURL;
+    public Button display;
+
     @FXML
-    private Label test;
+    protected void addGame(){
+        String machineName = addMachineName.getText();
+        String manufacturer = addManufacturer.getText();
+        String type = addType.getText();
+        String media = addMedia.getText();
+        int yearOfRelease = Integer.parseInt(addYearOfRelease.getText());
+        float price = Float.parseFloat(addPrice.getText());
+        String URL = addURL.getText();
+
+        GamesMachine gm = new GamesMachine(machineName, manufacturer, type, media, yearOfRelease, price, URL);
+        gm.setMachineName(machineName);
+        gm.setManufacturer(manufacturer);
+        gm.setType(type);
+        gm.setMedia(media);
+        gm.setYearOfLaunch(yearOfRelease);
+        gm.setPrice(price);
+        gm.setUrl(URL);
+
+        allGamesMachines.add(gm);
+
+        addMachineName.clear();
+        addManufacturer.clear();
+        addType.clear();
+        addMedia.clear();
+        addYearOfRelease.clear();
+        addPrice.clear();
+        addURL.clear();
 
 
+    }
 
+    @FXML
+    protected void displayGameMachines(){
+        display.setText(allGamesMachines.display());
+    }
 }
